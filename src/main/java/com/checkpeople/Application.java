6fd23ec324d68b637acf53dafb520a2aefda3dea
Joao Paulo <jpbaterabsb@gmail.com>
2019-01-18 02:50:57 -0200

    heroku default port

--- src/main/java/com/checkpeople/Application.java
+++ src/main/java/com/checkpeople/Application.java
@@ -7,8 +7,16 @@ import com.checkpeople.route.WebRoute;
 public class Application {
 
 	public static void main(String[] args) {	
-		port(8085); 
+		port(getHerokuAssignedPort()); 
 		WebRoute.start();
 
+    }
+	
+	static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
     }
 }
